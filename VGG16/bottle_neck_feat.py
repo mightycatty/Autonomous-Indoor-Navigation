91459577936715fedfb852fc3ae0e40838b4054b
yyeboah <yeboahjunior@yahoo.com>
2018-04-14 15:40:41 +0800

    Initial commit

---
+++ VGG16/bottle_neck_feat.py
@@ -0,0 +1,53 @@
+'''
+This code is solely for the purpose of extracting the bottle-neck features
+using the VGG-16 model without the top full connect layer
+ '''
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras.utils.np_utils import to_categorical
+from keras import applications
+
+# dimensions of our images.
+img_width, img_height = 224, 224
+
+top_model_weights_path = 'bottleneck_fc_model.h5'
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 1800
+nb_validation_samples = 720
+epochs = 50
+batch_size = 16
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size+1)
+    np.save(open('bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
+
+save_bottlebeck_features()
+
